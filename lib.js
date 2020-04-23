3dbdf7b13f41a62fb5cee3cfe87c968f92d4e287
Phil Toms <phil.toms@hotmail.co.uk>
2020-04-23 18:30:35 +0100

    chore add build script

--- lib.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-const circuitState = (m => m.__esModule ? /* istanbul ignore next */ m.default : /* istanbul ignore next */ m)(require('./index.js'));
-
-describe('circuit-state', () => {
-  it('should create a circuit', () => {
-    expect(circuitState({ x: jest.fn() })).toEqual('');
-  });
-});
